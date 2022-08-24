a32519fa0504ffc630f57b4fdc5540398edfcb43
ZeN <zensmi@yandex.ru>
2022-08-24 17:12:34 +0300

    DZ

---
+++ DZ_6/DZ_01/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+
+Console.Write($"Введи число М: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] Numbers = new int[m];
+
+void InputNumbers(int m)
+{
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    Numbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+int Comparison(int[] massNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massNumbers.Length; i++)
+  {
+    if(massNumbers[i] > 0 ) count += 1;
+  }
+  return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Чисел больше 0: {Comparison(Numbers)} ");
